fd7167a05f78b49f091af15753a4bd7fc235ace5
Marco <heimab13@htl-kaindorf.ac.at>
2017-03-21 08:31:12 +0100

    added class LightStreet

---
+++ src/main/java/com/mcmm/blocks/lightstreet/LightStreet.java
@@ -0,0 +1,43 @@
+package com.mcmm.blocks.lightstreet;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * Created by Marco on 20.03.2017.
+ */
+public class LightStreet extends Block {
+
+    public LightStreet() {
+        super(Material.ROCK);
+        this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.SOLID;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState iBlockState) {
+        return true;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState iBlockState) {
+        return true;
+    }
+
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState iBlockState) {
+        return EnumBlockRenderType.MODEL;
+    }
+
+}
